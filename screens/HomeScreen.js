6411b46e2d4651eddf2c8d2bc0585ef97911b3c2
Pranjal Gore <jalispran@gmail.com>
2023-05-30 15:14:21 +0000

    Add Search Button to HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import {ScrollView, View, Text, FlatList} from 'react-native';
+import { ScrollView, View, Text, FlatList } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import RECIPES from '../assets/recipes.json'
 import RECIPES_V2 from '../assets/Indian Food Recipe Dataset - IndianFoodDataset8.json'
@@ -7,6 +9,17 @@ import Item from '../components/Item'
 
 
 export const HomeScreen = ({navigation}) => {
+
+  useEffect(() => {
+    navigation.setOptions({ 
+      headerRight: () => <MaterialIcons style={{
+        flex: 1,
+        padding: 20,
+        alignItems: 'center',
+    }} name="search" size={24} color="black" onPress={() => console.log('Btn pressed')}/>
+    })
+  }, [])
+
     return (
       <ScrollView>
         <View style={{
